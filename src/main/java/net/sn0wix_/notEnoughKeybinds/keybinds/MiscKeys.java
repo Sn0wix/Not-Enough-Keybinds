6093f4ed2dfe4f5012633e51bce8ea71bbc04bed
Sn0wix_ <100717363+Sn0wix@users.noreply.github.com>
2025-12-28 20:05:49 +0100

    another version error fix

--- src/main/java/net/sn0wix_/notEnoughKeybinds/keybinds/MiscKeys.java
+++ src/main/java/net/sn0wix_/notEnoughKeybinds/keybinds/MiscKeys.java
@@ -1,7 +1,13 @@
 package net.sn0wix_.notEnoughKeybinds.keybinds;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.GameMenuScreen;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.gui.screen.MessageScreen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
+import net.minecraft.text.Text;
 import net.sn0wix_.notEnoughKeybinds.NotEnoughKeybinds;
 import net.sn0wix_.notEnoughKeybinds.keybinds.custom.KeybindCategory;
 import net.sn0wix_.notEnoughKeybinds.keybinds.custom.NotEKKeyBinding;
@@ -10,11 +16,30 @@ public class MiscKeys extends NotEKKeyBindings{
     public static final String MISC_CATEGORY_KEY = NotEnoughKeybinds.getIdentifier("misc").toTranslationKey("key.category");
 
     public static final NotEKKeyBinding DISCONNECT = registerModKeyBinding(new NotEKKeyBinding("disconnect", MISC_CATEGORY_KEY, (client, keyBinding) -> {
-        client.getAbuseReportContext().tryShowDraftScreen(client, new GameMenuScreen(true), () -> GameMenuScreen.disconnect(client, ClientWorld.QUITTING_MULTIPLAYER_TEXT), true);
+        client.getAbuseReportContext().tryShowDraftScreen(client, new GameMenuScreen(true), () -> disconnectWorld(client), true);
     }));
 
     @Override
     public KeybindCategory getCategory() {
         return new KeybindCategory(MISC_CATEGORY_KEY, 2, false, DISCONNECT);
+    }
+
+    public static void disconnectWorld(MinecraftClient client) {
+        boolean bl = client.isInSingleplayer();
+        ServerInfo serverInfo = client.getCurrentServerEntry();
+        client.world.disconnect();
+        if (bl) {
+            client.disconnect(new MessageScreen(Text.translatable("menu.savingLevel")));
+        } else {
+            client.disconnect();
+        }
+        TitleScreen titleScreen = new TitleScreen();
+        if (bl) {
+            client.setScreen(titleScreen);
+        } else if (serverInfo != null && serverInfo.isRealm()) {
+            client.setScreen(new RealmsMainScreen(titleScreen));
+        } else {
+            client.setScreen(new MultiplayerScreen(titleScreen));
+        }
     }
 }
